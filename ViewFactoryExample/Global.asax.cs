3911d1d9466896400f3f4b9ad73ef9c8d8c46948
Doug Mason <dmason@unitedfiregroup.com>
2017-08-17 19:11:31 -0500

    Added basic ASP.NET MVC.

---
+++ ViewFactoryExample/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Routing;
+using ViewFactoryExample.App_Start;
+
+namespace ViewFactoryExample
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
